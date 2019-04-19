07e49498637c3af89ca3f7a130b34e96491de49c
Adrian Brinoes <Coding4Adrian@yahoo.com>
2019-04-18 20:40:18 -0700

    css

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import Header from "./components/Header";
 import Instructions from "./components/Instructions";
 import Counter from "./components/Counter";
+import "./App.css"
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="body">
         <Header>Minion Mayhem</Header>
         <Instructions><strong>Instructions:</strong> Click on a Minion, but REMEMBER which one it is. You must pick a Minion without repeating yourself. You will gain <strong>1 point for each new Minion</strong> you click. But if <strong>you repeat yourself, you lose the game</strong> and your score is back at 0.  Can you track these mischievous Minions and beat your High Score?!?!?!</Instructions>
         <Counter />
