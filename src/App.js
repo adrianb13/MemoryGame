5adf8e710fcfe577f62b72edb00d51cee3f1d32d
Adrian Brinoes <Coding4Adrian@yahoo.com>
2019-04-19 10:16:46 -0700

    footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from "./components/Header";
 import Instructions from "./components/Instructions";
 import Counter from "./components/Counter";
+import Footer from "./components/Footer";
 import "./App.css"
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
         <Header>Minion Mayhem</Header>
         <Instructions><strong>Instructions:</strong> Click on a Minion, but REMEMBER which one it is. You must pick a Minion without repeating yourself. You will gain <strong>1 point for each new Minion</strong> you click. But if <strong>you repeat yourself, you lose the game</strong> and your score is back at 0.  Can you track these mischievous Minions and beat your High Score?!?!?!</Instructions>
         <Counter />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+class Footer extends React.Component {
+  render() {
+    return ( 
+      <div className="footer">
+        By Adrian Briones: <a href="https://adrianb13.github.io/Responsive-Portfolio/">Click Here For More</a>
+      </div>
+    )
+  }
+}
+
+export default Footer;
