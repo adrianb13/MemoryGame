e065e5d704bf39e8c8acdf73f1bae2d953a7297d
Adrian Brinoes <Coding4Adrian@yahoo.com>
2019-06-10 22:55:29 -0700

    Updates

---
+++ precache-manifest.4eca3235b2bcb5c32d50fd09afd4afc2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "29ace23a3c02840c5e90946f79b07843",
+    "url": "/MemoryGame/static/media/gru.29ace23a.jpg"
+  },
+  {
+    "revision": "2cc164899ca3c47b4117428babc8ccb3",
+    "url": "/MemoryGame/static/media/drNefario.2cc16489.jpg"
+  },
+  {
+    "revision": "97a9f2617e74c3fc05a141482e31f4ac",
+    "url": "/MemoryGame/static/media/background.97a9f261.jpg"
+  },
+  {
+    "revision": "4c3bd79ed0e125202499",
+    "url": "/MemoryGame/static/js/runtime~main.d54f135b.js"
+  },
+  {
+    "revision": "384ea9d5d76e7294a6d7",
+    "url": "/MemoryGame/static/js/main.dd163651.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/MemoryGame/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "384ea9d5d76e7294a6d7",
+    "url": "/MemoryGame/static/css/main.7b7f933f.chunk.css"
+  },
+  {
+    "revision": "aa4cdcb345f278d78d1b10ed38e16427",
+    "url": "/MemoryGame/index.html"
+  }
+];
